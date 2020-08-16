61ae71e5f8c2016506824bea81bf72f13ada1698
Sean <seantraut@gmail.com>
2020-08-16 16:35:38 -0400

    Image enlarge is functional, needs scrolling

--- bizzyclone.js
+++ bizzyclone.js
@@ -1,5 +1,6 @@
 let selected_images = document.querySelectorAll(".image");
 let image_display = document.querySelector("image-display");
+let grow_image = document.querySelector("grow-image");
 
 for(let image of selected_images){
     image.onclick = expand_image;
@@ -7,9 +8,10 @@ for(let image of selected_images){
 
 function expand_image(event){
     let image = event.currentTarget;
-    let source = image.getAttribute('source');
+    let source = `url('${image.getAttribute('source')}')`;
     
-    console.log(image, source);
+    console.log(image_display, image, source);
 
-    image_display.setAttribute("display", "flex");
+    image_display.style.display = "flex";
+    grow_image.style.backgroundImage = source;
 };
