af06bfc1b223c39a73363050f4305df8f569f6ae
Sean <seantraut@gmail.com>
2020-08-21 15:10:22 -0400

    Expanded on basic data storage

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -66,9 +66,9 @@ let myCategory:Category = {
   product: [myProduct, {title: "drinking-water", price: 9999, source: "mother-earth", category: []}]
 }
 
-let products:Product[] = [
-  /*make_product("Neon Pink", 15.00, "http://placekitten.com/900/900"),
+let products:Product[] = [];
   
+/*
 {
   source: "http://placekitten.com/901/901",
   title: "Leaf Scarf Tie",
@@ -103,8 +103,7 @@ let products:Product[] = [
   source: "http://placekitten.com/907/907",
   title: "Peach",
   price: 15.00,
-},  */
-];
+}*/
 
 /* === Functions === */
 
@@ -163,12 +162,34 @@ function make_product(title:string, price:number, source:string,
   let product:Product = {title, price, source, category: categories, featured, instock};
   
   for(let category of categories){
-      category.product.push(product);
+    category.product.push(product);
   }
 
   products.push(product);
   return product;
 }
 
+function make_category(type:string, title:string, source:string, products:Product[] = []){
+
+  let category:Category = {type, title, source, product: products};
+
+  for(let product of products){
+    product.category.push(category);
+  }
+
+  //push to a list of categories when needed
+  return category;
+}
+
+let neon_pink = make_product("Neon Pink", 15.00, "http://placekitten.com/900/900");
+let leaf_scarf_tie = make_product("Leaf Scarf Tie", 15.00, "http://placekitten.com/901/901");
+let fuschia_scarf_tie = make_product("Fuschia Scarf Tie", 15.00, "http://placekitten.com/902/902");
+let turquoise_scarf_tie = make_product("Turquoise Scarf Tie", 15.00, "http://placekitten.com/903/903");
+let retro_dot_scarf_tie = make_product("Retro Dot Scarf Tie", 15.00, "http://placekitten.com/904/904");
+let ikat_scarf_tie = make_product("Ikat Scarf Tie", 15.00, "http://placekitten.com/905/905");
+let kelly_greem = make_product("Kelly Green", 15.00, "http://placekitten.com/906/906");
+let peach = make_product("Peach", 15.00, "http://placekitten.com/907/907");
+
+console.log(make_category("Scarf Tie", "Scarf Tie", "url", [leaf_scarf_tie, fuschia_scarf_tie, turquoise_scarf_tie, retro_dot_scarf_tie, ikat_scarf_tie]));
 
 export default App;
